d67d9a36ae96e112b719301a53ccccd87d9d34f0
rosa24 <rosa24@student.bth.se>
2025-10-30 13:03:22 +0100

    Read socker URL from env

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL || import.meta.env.VITE_API_URL || "";
+
+export const socket = io(SOCKET_URL || "http://localhost:3001");
 
 socket.on("connect", () => {
   console.log("Socket ansluten, id:", socket.id);
